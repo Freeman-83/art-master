f504236acd0e11e94b42d81b5a0137b134d80a99
Михаил Марин <mmn-83@yandex.ru>
2023-12-14 23:51:03 +0300

    Installed psycorg2. Changed using DB

--- art_master_backend/art_master_backend/settings.py
+++ art_master_backend/art_master_backend/settings.py
@@ -87,14 +87,6 @@ WSGI_APPLICATION = 'art_master_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# django.db.backends.postgresql
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
